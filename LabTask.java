babb0bc942db919dbfe0f5a5f4ff6f4f65aa415a
ArainIhtisham <49509517+ArainIhtisham@users.noreply.github.com>
2019-04-11 10:50:48 +0500

    Add files via upload

---
+++ LabTask.java
@@ -0,0 +1,17 @@
+  
+  class LabTask{
+	public static void main (String args[])
+		{
+			string x=args[0];
+			string y=args[1];
+		    string z=args[2];
+	int i=Integer.ParseInt(x);		  
+	int j=Integer.ParseInt(y);
+	int k=Integer.ParseInt(z);
+    
+    System.out.println(x+y+z);
+    System.out.println(i+j+k);
+
+ }
+
+}
